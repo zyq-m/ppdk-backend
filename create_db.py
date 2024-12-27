e3b6b8e08a04493d1be5547685f170ff6f10cbc2
haziq <haziq.musa02@gmail.com>
2024-12-27 18:37:51 +0800

    create seeder

--- create_db.py
+++ create_db.py
@@ -1,4 +1,34 @@
 from app import app, db
+from model import Admin, Jantina, Role, PPDK, Phone
+from extensions import f_bcrypt
+
+
+def seed_db():
+    db.create_all()
+
+    superAdmin = Role(name="super-admin")
+    admin = Role(name="admin")
+    pelatih = Role(name="pelatih")
+
+    lelaki = Jantina(jantina="Lelaki")
+    perempuan = Jantina(jantina="Perempuan")
+
+    ppdk = PPDK(nama="Super PPDK", alamat="Super PPDK")
+    superAcc = Admin(
+        email="admin@ppdk.com",
+        nama="Super",
+        jawatan="Super Admin",
+        ppdk=ppdk,
+        password=f_bcrypt.generate_password_hash("ppdk2024"),
+        role=superAdmin,
+    )
+    superPhone = Phone(no_tel="01119650612", admin=superAcc)
+
+    db.session.add_all(
+        [superAdmin, admin, pelatih, lelaki, perempuan, ppdk, superAcc, superPhone]
+    )
+    db.session.commit()
+
 
 with app.app_context():
-    db.create_all()
+    seed_db()
